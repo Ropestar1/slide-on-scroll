36f51a33a97e63bc05c7a38ebb6fd86a8b827b89
HQ <5462117+Ropestar1@users.noreply.github.com>
2017-09-25 13:46:43 -0700

    Slide completed.

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,29 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderImages.forEach(sliderImage => {
+    // half way through image
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    // bottom of image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    // NOTE: good practice to list conditional in a descriptive variable to remember what you were doing. Makes things clearer.
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    }
+    else {
+      sliderImage.classList.remove('active');
+    }
+
+  })
+
+}
+
+// debounce function makes sure scroll listener isn't always on
+window.addEventListener('scroll', debounce(checkSlide));
